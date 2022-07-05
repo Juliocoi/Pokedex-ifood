92b3b49e08e34fafd5c1f848a7f0d54f925a0a25
Juliocoi <amorim.jc@gmail.com>
2022-07-05 16:05:17 -0300

    add métodos put and delete em legendaries

--- src/app/controllers/LegendaryController.js
+++ src/app/controllers/LegendaryController.js
@@ -1,22 +1,30 @@
-const LegendariesService = require('../services/LegendariesService');
+const ListLegendariesService = require('../services/ListLegendariesService');
 const CreateLegendaryService = require('../services/CreateLegendaryService');
 const { response } = require('express');
+const UpdateLegendaryService = require('../services/UpdateLegendaryService');
+const DeleteLegendaryService = require('../services/DeleteLegendaryService');
+
 
 const controller = {
   index: (request, response) => {
-    const listLegendaries = LegendariesService.listLegendariesService()
+    const listLegendaries = ListLegendariesService.listLegendariesService()
     response.json(listLegendaries)
   },
 
   ListData: (request, response) => {
     const { name } = request.query;
-    const legendary = LegendariesService.listPokemonData(name);
+
+    if (!name) {
+      return response.status(400).json({ "Erro": "Você precisa passar um nome válido" });
+    }
+
+    const legendary = ListLegendariesService.listPokemonData(name);
 
     return response.json(legendary)
   },
 
   create: (request, response) => {
-     
+
     const {
       name,
       description,
@@ -48,7 +56,44 @@ const controller = {
 
   },
 
+  update: (request, response) => {
+    const { id } = request.params;
+    const {
+      name,
+      description,
+      type,
+      healthPoints,
+      specialAttack,
+      defense,
+      attack,
+      experience,
+      specialDefense
+    } = request.body;
 
-};
+    const updateLegendary = UpdateLegendaryService.update(
+      id,
+      name,
+      description,
+      type,
+      healthPoints,
+      specialAttack,
+      defense,
+      attack,
+      experience,
+      specialDefense
+    )
+
+    response.send(updateLegendary)
+  },
+
+  delete: (request, response) => {
+    const { id } = request.params;
+    const resultado = DeleteLegendaryService.delete(id);
+
+    response.send(resultado)
+  }
+
+
+}
 
 module.exports = controller;
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,9 @@ const controller = require('./app/controllers/LegendaryController');
 const routes = new Router();
 
 //routes.get('/legendaries', controller.index);
-routes.get('/legendaries', controller.ListData)
-routes.post('/legendaries', controller.create)
+routes.get('/legendaries', controller.ListData);
+routes.post('/legendaries', controller.create);
+routes.put('/legendaries/:id', controller.update);
+routes.delete('/legendaries/:id', controller.delete);
 
 module.exports = routes;
